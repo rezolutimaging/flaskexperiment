7d60f1a44e87f357ecd72b561cf1c56a9e1794f7
rezolutimaging <evan.valentini@rezolut.com>
2020-03-14 18:34:14 -0400

    started building udacity class example app

---
+++ flask-hello-app.py
@@ -0,0 +1,24 @@
+from flask import Flask 
+from flask_sqlalchemy import SQLAlchemy
+import psycopg2
+
+app = Flask(__name__) 
+
+#set configuration variable to connect to database
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres@localhost:5432/example'  
+
+#initialize sqlalchemy database 
+db = SQLAlchemy(app)
+
+#create a person model 
+class Person(db.Model): 
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(), nullable=False)
+
+#create database tables 
+db.create_all()
+
+
+@app.route('/')
+def index():
+    return 'hello world'
